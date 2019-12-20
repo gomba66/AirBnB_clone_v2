4d40de21a0f2fbb8fbdf986e64f2b77fa546ecb0
sechchr22 <980@holbertonschool.com>
2019-12-20 16:47:46 +0000

    State Class

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This is the state class"""
 
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -11,23 +13,28 @@ class State(BaseModel, Base):
         name: input name
     """
 
+    type_of_storage = os.getenv('HBNB_TYPE_STORAGE')
+
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='all, delete')
 
-    @property
-    def cities(self):
-        """Getter attritube
-        Return:
-        list of City instances with state_id equals 
-        to the current State.id
-        """
-        cities = []
-        obj_dict = storage.all(City)
-        keys = obj_dict.keys()
+    if type_of_storage == 'db':
+        cities = relationship("City", backref="state", cascade='all, delete')
 
-        for key in keys:
-            if self.id in keys:
-                cities.append(obj_dict[key])
-        
-        return cities
+    else:
+        @property
+        def cities(self):
+            """Getter attritube
+            Return:
+            list of City instances with state_id equals
+            to the current State.id
+            """
+            cities = []
+            obj_dict = storage.all(City)
+            keys = obj_dict.keys()
+
+            for key in keys:
+                if self.id in key:
+                    cities.append(obj_dict[key])
+
+            return cities
