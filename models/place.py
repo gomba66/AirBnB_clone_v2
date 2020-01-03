b79e72df4338131423a043c0630da82476d0dce9
sechchr22 <980@holbertonschool.com>
2020-01-03 23:46:02 +0000

    updated

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the place class"""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from sqlalchemy.orm import relationship
