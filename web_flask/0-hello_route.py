4c36fc455914ecba2980f3886ae8f9138c70c76d
gomba66 <danielalejo66@hotmail.com>
2020-01-22 18:27:12 +0000

    I added the first files of the flask project

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains a flask web inicialization
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def display_r():
+    """
+    Function that display a message when the flask service is online
+    """
+    return("Hello HBNB")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
