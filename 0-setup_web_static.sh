46424db860cd8e218fc29785bed87de5ddcb2884
gomba66 <danielalejo66@hotmail.com>
2020-03-02 03:44:17 +0000

    I fixed my file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,13 @@
 #!/usr/bin/env bash
-# Setting webservers to be ready for the web_static deployment
-
-# installing Ngnix
+# Script that configures Nginx server with some folders and files
 apt-get -y update
 apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-echo "Holberton School" > /var/www/html/index.nginx-debian.html
 service nginx start
-
-# creating directories if not exists
-for DIRECTORIES in /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-do
-	if [[ ! -d $DIRECTORIES ]]
-	then
-		mkdir $DIRECTORIES
-	fi
-done
-
-# creating fake HTML to test Nginx config
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-# creating symbolic linked to /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group
-chown -R ubuntu /data/
-chown -R :ubuntu /data/
-
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-old_string="# include snippets/snakeoil.conf;"
-new_string="\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i "s%$old_string%$new_string%" /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
+exit 0
