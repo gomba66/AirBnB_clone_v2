3baf326dc78ed269c0822e5618aee420d808cfb5
gomba66 <danielalejo66@hotmail.com>
2020-03-02 04:21:35 +0000

    I fixed a little error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,7 @@ def do_pack():
             local('mkdir versions')
         t = datetime.now()
         f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strtime(f))
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
         local('tar -cvzf {} web_static'.format(archive_path))
         return archive_path
     except:
